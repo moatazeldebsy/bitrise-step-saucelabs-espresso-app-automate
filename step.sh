4f69179f961a10b868f66cb049f827137f2bb5fd
moatazeldebsy <moataznabil2019@gmail.com>
2019-11-08 23:42:16 +0100

    repair step

--- step.sh
+++ step.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -ex
+echo "Downloading Saucelabs Runner for Virtual Devices"
 
-echo "starting automated tests"
 wget https://saucelabs.com/downloads/sauce-runner-virtual-0.1.1-linux.zip
 
 unzip sauce-runner-virtual-0.1.1-linux.zip
+
+echo "starting uploading app and test apk to Saucelabs and run the automated tests"
+
 ./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCE_USERNAME  -k $SAUCE_ACCESS_KEY \
-  -f $test_framework \
+  -f espresso \
   -a $BITRISE_APK_PATH \
   -t $BITRISE_TEST_APK_PATH \
-  -d "$saucelabs_devices" \
-  --data-center $data_center
+  --devices "${SAUCELABS_DEVICE_LIST}" \
+  --data-center $DATA_CENTER
