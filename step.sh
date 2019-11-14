c69588dbb400660c476d788bb22b0b80b4f4090d
moatazeldebsy <moataznabil2019@gmail.com>
2019-11-14 15:13:50 +0100

    fix data center and devices arguments

--- step.sh
+++ step.sh
@@ -8,9 +8,6 @@ unzip sauce-runner-virtual-0.1.1-linux.zip
 
 echo "starting uploading app and test apk to Saucelabs and run the automated tests"
 
-./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCE_USERNAME  -k $SAUCE_ACCESS_KEY \
-  -f espresso \
-  -a $BITRISE_APK_PATH \
-  -t $BITRISE_TEST_APK_PATH \
-  --devices "${SAUCELABS_DEVICE_LIST}" \
-  --data-center $DATA_CENTER
+./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
+  -f espresso -a $BITRISE_APK_PATH \
+  -t $BITRISE_TEST_APK_PATH --devices "${saucelabs_device_list}" --data-center ${saucelabs_data_center}
