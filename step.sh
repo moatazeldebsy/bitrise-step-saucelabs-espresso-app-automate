193b62691cdc12e73c3668c56db6f8171f724b31
moatazeldebsy <moataznabil2019@gmail.com>
2019-11-26 16:57:41 +0100

    fix agruments for saucelab commandline tool

--- step.sh
+++ step.sh
@@ -10,4 +10,4 @@ echo "starting uploading app and test apk to Saucelabs and run the automated tes
 
 ./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
   -f espresso -a $BITRISE_APK_PATH \
-  -t $BITRISE_TEST_APK_PATH --devices "${saucelabs_device_list}" --data-center ${saucelabs_data_center}
+  -t $BITRISE_TEST_APK_PATH -d "deviceName=${saucelabs_device_name}" --data-center ${saucelabs_data_center}
